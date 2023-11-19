d6fc0c4a6edb7fefab75b53657774d533cf014b7
AP <145212232+MajinAP@users.noreply.github.com>
2023-11-18 19:04:21 -0500

    Update 04-NAT.tf

--- 04-NAT.tf
+++ 04-NAT.tf
@@ -8,10 +8,11 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public-eu-west-1a.id
+  subnet_id     = aws_subnet.public-us-east-2a.id
 
   tags = {
     Name = "nat"
+    Owner = "MajinVegeta"
   }
 
   depends_on = [aws_internet_gateway.igw]
