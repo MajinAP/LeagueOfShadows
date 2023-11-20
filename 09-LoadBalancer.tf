818452437469389d2d3bd81360ed84cb5f471cb4
AP <145212232+MajinAP@users.noreply.github.com>
2023-11-19 23:12:23 -0500

    Update 09-LoadBalancer.tf
    remo/theo balerica 443 resource

--- 09-LoadBalancer.tf
+++ 09-LoadBalancer.tf
@@ -23,7 +23,20 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
- 
+data "aws_acm_certificate" "cert" {
+  domain   = "balericamalgusADD.com"
+  statuses = ["ISSUED"]
+  most_recent = true
+}
+
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app1_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"  # or whichever policy suits your requirements
+  certificate_arn   = data.aws_acm_certificate.cert.arn
+
 data "aws_acm_certificate" "cert" {
   domain   = "revanwar.com"
   statuses = ["ISSUED"]
